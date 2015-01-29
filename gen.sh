ac3ce5ff8c722e4d4742097d5f832545bfdf5f26
Denny Huang <denny0223@gmail.com>
2015-01-30 03:35:02 +0800

    Add generate kml file shell script

---
+++ gen.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+hostname=https://example.com/kml/
+filepath=/var/www/html/kml/
+filename=$(date +"%Y%m%d%H%M%S").kml
+
+cat template_head >> $filename
+node index.js $1 >> $filename
+cat template_tail >> $filename
+
+mv $filename $filepath
+
+echo https://maps.google.com/maps?q=$hostname$filename
+
